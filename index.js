7edfe65f2075f29f8f490c4c931f253a4535438d
mdumouchel <MrBunny@MrBunnys-MacBook-Pro.local>
2015-05-01 07:01:12 -0700

    Added Knockout and figured out the strange observableArray issue.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $( document ).ready(function(){
 	//Notice accessing array elements this is important in understanding how the array works.
 	//Notice at the bottom has a series of different properties and methods that can be used with arrays
 	//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-	var arrayOfItems = ["A", "B", "C"];
+	/*var arrayOfItems = ["A", "B", "C"];
 
 	//For loop documentation
 	//http://www.w3schools.com/js/js_loop_for.asp
@@ -22,5 +22,48 @@ $( document ).ready(function(){
 
 		//http://api.jquery.com/append/
 		$("#itemsToLoop").append(div)
+	}*/
+	var viewModel = {
+		bitStrings: ko.observableArray(),
+		daString: ko.observableArray([ {value: ko.observable("X")}, {value: ko.observable("X")}, {value: ko.observable("X")} ])
+		//The following dont work...
+		//daString: ko.observableArray([ko.observable("0"), ko.observable("0"), ko.observable("0")])
+		//daString: ko.observableArray([0,0,0])
+		//daString: ko.observableArray([{value: 0}, {value: 0}, {value: 0}])
+		//daString: ko.observableArray(ko.observable({value: 0}), ko.observable({value: 0}), ko.observable({value: 0}))
 	}
+
+	for(var i=0; i < 3; i++) {
+		viewModel.bitStrings.push({ 
+            	firstName: ko.observable('Bert'), 
+            	numberOfBits: ko.observable(4),
+            		daString: ko.observableArray(["0", "0", "0"])
+        	
+            	//daString: ko.observableArray([{value: "0"}, {value: "0"}, {value: "0"}])
+        	});
+
+		/*viewModel.bitStrings()[viewModel.bitStrings().length - 1].daString = ko.observableArray();
+		for(var s=0; s < 4; s++) {
+			viewModel.bitStrings[viewModel.bitStrings().length - 1].daString().push("1");
+		}*/
+	}
+		/*
+        bitStrings: [
+            { 
+            	firstName: ko.observable('Bert'), 
+            	numberOfBits: ko.observable(16) 
+        	},
+            { 
+            	firstName: ko.observable('charles'), 
+            	numberOfBits: ko.observable(16)  
+            },
+            { 
+            	firstName: ko.observable('charles2'), 
+            	numberOfBits: ko.observable(16)
+            }
+        ]
+    }*/
+
+
+	ko.applyBindings(viewModel);
 })
